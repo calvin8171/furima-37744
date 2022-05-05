d26d04688dd5373ca5190e40f52f2dca2100e04a
calvin8171 <calvin8171@gmail.com>
2022-05-05 20:54:59 +0900

    商品購入機能テストコード first commit

--- spec/factories/purchase_record_shipping_infos.rb
+++ spec/factories/purchase_record_shipping_infos.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :purchase_record_shipping_info do
-    
+    postal_code { '123-4567' }
+    prefecture_id { 2 }
+    municipalities { '東京都' }
+    banchi { '1-2-3' }
+    tatemono_name { '東京ハイツ' }
+    phone { '08012345678' }
   end
 end
+
--- spec/models/purchase_record_shipping_info_spec.rb
+++ spec/models/purchase_record_shipping_info_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseRecordShippingInfo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '購入内容の確認' do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @purchase_record_shipping_info = FactoryBot.build(:purchase_record_shipping_info, user_id: user.id, item_id: item.id)
+    end
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@purchase_record_shipping_info).to be_valid
+      end
+      it 'tatemono_nameは空でも保存できること' do
+        @purchase_record_shipping_info.tatemono_name = ''
+        expect(@purchase_record_shipping_info).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'postal_codeが空だと保存できないこと' do
+        @purchase_record_shipping_info.postal_code = ''
+        @purchase_record_shipping_info.valid?
+        expect(@purchase_record_shipping_info.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+        @purchase_record_shipping_info.postal_code = '1234567'
+        @purchase_record_shipping_info.valid?
+        expect(@purchase_record_shipping_info.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+      end
+      it 'prefecture_idを選択していないと保存できないこと' do
+        @purchase_record_shipping_info.prefecture_id = 1
+        @purchase_record_shipping_info.valid?
+        expect(@purchase_record_shipping_info.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'municipalitiesが空だと保存できないこと' do
+        @purchase_record_shipping_info.municipalities = ''
+        @purchase_record_shipping_info.valid?
+        expect(@purchase_record_shipping_info.errors.full_messages).to include("Municipalities can't be blank")
+      end
+      it 'banchiが空だと保存できないこと' do
+        @purchase_record_shipping_info.banchi = ''
+        @purchase_record_shipping_info.valid?
+        expect(@purchase_record_shipping_info.errors.full_messages).to include("Banchi can't be blank")
+      end
+      it 'phoneが空だと保存できないこと' do
+        @purchase_record_shipping_info.phone = ''
+        @purchase_record_shipping_info.valid?
+        expect(@purchase_record_shipping_info.errors.full_messages).to include("Phone can't be blank")
+      end
+      it 'userが紐付いていないと保存できないこと' do
+        @purchase_record_shipping_info.user_id = nil
+        @purchase_record_shipping_info.valid?
+        expect(@purchase_record_shipping_info.errors.full_messages).to include("User can't be blank")
+      end
+      it 'itemが紐付いていないと保存できないこと' do
+        @purchase_record_shipping_info.item_id = nil
+        @purchase_record_shipping_info.valid?
+        expect(@purchase_record_shipping_info.errors.full_messages).to include("Item can't be blank")
+      end
+    end
+  end
 end
